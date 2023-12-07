a90cc0be7b5ba7fff6feecfafd824fb767827fe7
AvadhootSmart <1234avadhootsmart@gmail.com>
2023-12-07 14:45:37 +0530

    ad

--- Gitacora_django/settings.py
+++ Gitacora_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hkwb%o65sprs)u(hsvhdt4m7rot4$36i8%y^eh4e5m-x(zfb)8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.1.4','192.168.252.90']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
